24011baf9764c46370638e2d35eeca4413598316
Alex Miller <alexpmil@gmail.com>
2015-06-08 11:13:54 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -14,3 +14,17 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-37920818-1', 'auto');
 ga('send', 'pageview');
 
+/* Track outbound links */
+var outbounds = $("a").filter(function(index){
+    try{
+        var h = $(this).attr("href");
+        return h.indexOf("http") > -1 && h.indexOf("smartypal.com") == -1;
+    } catch(err){
+        return false;
+    }
+});
+$(outbounds).click(function(){
+    ga('send', 'event', 'Outbound Link', $(this).attr("href"), 'From page: '.concat(document.URL) );
+    console.log($(this).attr("href"))
+});
+
