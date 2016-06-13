b20439b2b11d7e8b9d2382f5d264a13135a22d79
alexpmil <alexpmil@gmail.com>
2016-06-13 18:00:54 +0000

    Open in PHL update

--- open-in-phl/mapvars.js
+++ open-in-phl/mapvars.js
@@ -6,7 +6,7 @@ function initMap() {
     center: philly
     });
 
-    var tweets = [{'lng': -75.16642017132394, 'name': 'Chhaya Cafe', 'lat': 39.92737451264627, 'content': '<div id="content"> <h1>Chhaya Cafe</h1> <div id="bodyContent"> <hr> <p><a target="_blank" href="https://twitter.com/chhayacafe">@chhayacafe</a>: you asked we answered! now open late #coffee #openinphl #chhayajunkie https://t.co/ixLkLk00Qy</p> <p style="font-size: 10px;">Tue Jun 07 14:20 | <a target="_blank" href="https://twitter.com/statuses/740186689553989633">Read on Twitter</a></p> <hr> <p> Phone: (215) 465-1000<br/> Directions: <a href="https://maps.google.com?daddr=1819+E+Passyunk+Ave%2C+Philadelphia%2C+PA+19148" target="_blank">1819 E Passyunk Ave, Philadelphia, PA 19148</a><br/> Website: <a target="_blank" href="http://chhayacafe.com">http://chhayacafe.com</a> </p> </div> </div>'}];
+    var tweets = [{'lat': 39.92737451264627, 'name': 'Chhaya Cafe', 'lng': -75.16642017132394, 'content': '<div id="content"> <h1>Chhaya Cafe</h1> <div id="bodyContent"> <hr> <p><a target="_blank" href="https://twitter.com/chhayacafe">@chhayacafe</a>: you asked we answered! now open late #coffee #openinphl #chhayajunkie https://t.co/ixLkLk00Qy</p> <p style="font-size: 10px;">Tue Jun 07 14:20 | <a target="_blank" href="https://twitter.com/statuses/740186689553989633">Read on Twitter</a></p> <hr> <p> Phone: (215) 465-1000<br/> Directions: <a href="https://maps.google.com?daddr=1819+E+Passyunk+Ave%2C+Philadelphia%2C+PA+19148" target="_blank">1819 E Passyunk Ave, Philadelphia, PA 19148</a><br/> Website: <a target="_blank" href="http://chhayacafe.com">http://chhayacafe.com</a> </p> </div> </div>'}];
 
     var contentStrings = [];
     var infowindow = [];
