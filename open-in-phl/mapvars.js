29de7f406f63358a0e89dbfcf54ee598dcf3fed6
alexpmil <alexpmil@gmail.com>
2016-08-08 18:00:46 +0000

    Open in PHL update

--- open-in-phl/mapvars.js
+++ open-in-phl/mapvars.js
@@ -6,7 +6,7 @@ function initMap() {
     center: philly
     });
 
-    var tweets = [{'lng': -75.71137905120848, 'content': '<div id="content"> <h1>PYKNIC</h1> <div id="bodyContent"> <hr> <p><a target="_blank" href="https://twitter.com/Pyknic">@Pyknic</a>: Putting on massive amounts of @PiperWai then headed to @2ndStFestival. Pyknic is #OpeninPHL today, 12-10pm on 2nd St near @GunnersRun.</p> <p style="font-size: 10px;">Sun Aug 07 12:30 | <a target="_blank" href="https://twitter.com/statuses/762264664432672768">Read on Twitter</a></p> <hr> <p> Phone: (visit website)<br/> Directions: <a href="https://maps.google.com?daddr=215+W+Lancaster+Ave%2C+Downingtown%2C+PA+19335" target="_blank">215 W Lancaster Ave, Downingtown, PA 19335</a><br/> Website: <a target="_blank" href="False">False</a> </p> </div> </div>', 'lat': 40.00219112908278, 'name': 'PYKNIC'}];
+    var tweets = [{'content': '<div id="content"> <h1>PYKNIC</h1> <div id="bodyContent"> <hr> <p><a target="_blank" href="https://twitter.com/Pyknic">@Pyknic</a>: Putting on massive amounts of @PiperWai then headed to @2ndStFestival. Pyknic is #OpeninPHL today, 12-10pm on 2nd St near @GunnersRun.</p> <p style="font-size: 10px;">Sun Aug 07 12:30 | <a target="_blank" href="https://twitter.com/statuses/762264664432672768">Read on Twitter</a></p> <hr> <p> Phone: (visit website)<br/> Directions: <a href="https://maps.google.com?daddr=215+W+Lancaster+Ave%2C+Downingtown%2C+PA+19335" target="_blank">215 W Lancaster Ave, Downingtown, PA 19335</a><br/> Website: <a target="_blank" href="False">False</a> </p> </div> </div>', 'lng': -75.71137905120848, 'lat': 40.00219112908278, 'name': 'PYKNIC'}];
 
     var contentStrings = [];
     var infowindow = [];
