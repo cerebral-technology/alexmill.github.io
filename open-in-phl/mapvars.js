0e6211544505cdfb45a326ad14fde6564791dd15
alexpmil <alexpmil@gmail.com>
2017-08-31 18:01:33 +0000

    Open in PHL update

--- open-in-phl/mapvars.js
+++ open-in-phl/mapvars.js
@@ -6,7 +6,7 @@ function initMap() {
     center: philly
     });
 
-    var tweets = [];
+    var tweets = [{'name': 'The Belgian Cafe', 'content': '<div id="content"> <h1>The Belgian Cafe</h1> <div id="bodyContent"> <hr> <p><a target="_blank" href="https://twitter.com/thebelgiancafe">@thebelgiancafe</a>: We are #OpeninPHL #LaborDayWeekend next to the @MIAFestival with #CraftBeer bottles and on @PhillyTapFinder https://t.co/TwvgqaWTOS</p> <p style="font-size: 10px;">Thu Aug 31 16:16 | <a target="_blank" href="https://twitter.com/statuses/903290461426733056">Read on Twitter</a></p> <hr> <p> Phone: (215) 235-3500<br/> Directions: <a href="https://maps.google.com?daddr=601+N.+21st+Street+%28at+Green+St%29%2C+Philadelphia%2C+PA+19130" target="_blank">601 N. 21st Street (at Green St), Philadelphia, PA 19130</a><br/> Website: <a target="_blank" href="http://www.thebelgiancafe.com">http://www.thebelgiancafe.com</a> </p> </div> </div>', 'lat': 39.965362315682, 'lng': -75.17257690429688}];
 
     var contentStrings = [];
     var infowindow = [];
