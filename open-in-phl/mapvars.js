6fe98cdc0a0c5e0786701cdc3f2c6bee5aea15b0
alexpmil <alexpmil@gmail.com>
2017-10-07 18:01:25 +0000

    Open in PHL update

--- open-in-phl/mapvars.js
+++ open-in-phl/mapvars.js
@@ -6,7 +6,7 @@ function initMap() {
     center: philly
     });
 
-    var tweets = [];
+    var tweets = [{'content': '<div id="content"> <h1>Philadelphia Museum of Art</h1> <div id="bodyContent"> <hr> <p><a target="_blank" href="https://twitter.com/philamuseum">@philamuseum</a>: The Museum is OPEN this Monday, Oct 9, for Columbus Day. Off from work or school? Stop by and visit your favorite w… https://t.co/PmZIsCVy8a</p> <p style="font-size: 10px;">Fri Oct 06 23:00 | <a target="_blank" href="https://twitter.com/statuses/916437945023913984">Read on Twitter</a></p> <hr> <p> Phone: (215) 763-8100<br/> Directions: <a href="https://maps.google.com?daddr=2600+Benjamin+Franklin+Pkwy%2C+Philadelphia%2C+PA+19130" target="_blank">2600 Benjamin Franklin Pkwy, Philadelphia, PA 19130</a><br/> Website: <a target="_blank" href="http://www.philamuseum.org">http://www.philamuseum.org</a> </p> </div> </div>', 'lng': -75.18106848275961, 'lat': 39.965705518332456, 'name': 'Philadelphia Museum of Art'}];
 
     var contentStrings = [];
     var infowindow = [];
