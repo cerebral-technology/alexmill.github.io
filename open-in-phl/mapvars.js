8a89404e1f9487cb5d1cd2ccbc6518b8cc31f678
alexpmil <alexpmil@gmail.com>
2017-11-04 18:01:44 +0000

    Open in PHL update

--- open-in-phl/mapvars.js
+++ open-in-phl/mapvars.js
@@ -6,7 +6,7 @@ function initMap() {
     center: philly
     });
 
-    var tweets = [{'name': 'The Chemical Heritage Foundation', 'content': '<div id="content"> <h1>The Chemical Heritage Foundation</h1> <div id="bodyContent"> <hr> <p><a target="_blank" href="https://twitter.com/ChemHeritage">@ChemHeritage</a>: We\'re #OpeninPHL during #PhillyFreeStreets today! https://t.co/Ad8Kz5XEx2</p> <p style="font-size: 10px;">Sat Oct 28 14:01 | <a target="_blank" href="https://twitter.com/statuses/924274839237660673">Read on Twitter</a></p> <hr> <p> Phone: (215) 925-2222<br/> Directions: <a href="https://maps.google.com?daddr=315+Chestnut+St+%28btwn+3rd+%26+4th+Sts%29%2C+Philadelphia%2C+PA+19106" target="_blank">315 Chestnut St (btwn 3rd & 4th Sts), Philadelphia, PA 19106</a><br/> Website: <a target="_blank" href="http://chemheritage.org">http://chemheritage.org</a> </p> </div> </div>', 'lng': -75.14643250883483, 'lat': 39.948726962168095}];
+    var tweets = [];
 
     var contentStrings = [];
     var infowindow = [];
