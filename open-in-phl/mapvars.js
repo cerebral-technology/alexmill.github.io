522856db7c43d61e69a23280ae655fa5408e4b3b
alexpmil <alexpmil@gmail.com>
2017-12-04 18:02:19 +0000

    Open in PHL update

--- open-in-phl/mapvars.js
+++ open-in-phl/mapvars.js
@@ -6,7 +6,7 @@ function initMap() {
     center: philly
     });
 
-    var tweets = [];
+    var tweets = [{'content': '<div id="content"> <h1>The Hungry Pigeon</h1> <div id="bodyContent"> <hr> <p><a target="_blank" href="https://twitter.com/hungry__pigeon">@hungry__pigeon</a>: Mmm...caffeine. 😴 Good morning Philly! Come and get it! \U0001f950🍳\U0001f953#mondaymotivation #openinphl @ Hungry… https://t.co/ojUPdaOeHK</p> <p style="font-size: 10px;">Mon Dec 04 12:32 | <a target="_blank" href="https://twitter.com/statuses/937660779565740032">Read on Twitter</a></p> <hr> <p> Phone: (215) 278-2736<br/> Directions: <a href="https://maps.google.com?daddr=743+S+4th+St+%28at+Fitzwater+St%29%2C+Philadelphia%2C+PA+19147" target="_blank">743 S 4th St (at Fitzwater St), Philadelphia, PA 19147</a><br/> Website: <a target="_blank" href="http://www.hungrypigeon.com">http://www.hungrypigeon.com</a> </p> </div> </div>', 'lat': 39.939137076872285, 'lng': -75.14969331618147, 'name': 'The Hungry Pigeon'}];
 
     var contentStrings = [];
     var infowindow = [];
